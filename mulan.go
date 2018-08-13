4c2dc06fccc7f26f39ee6235c902a7bf60aab85d
yuyang <yuyang@xiaoyun.com>
2018-08-13 10:15:14 +0800

    ✨ Feat: impl middleware iter

--- mulan.go
+++ mulan.go
@@ -10,46 +10,102 @@ type Handler interface {
 	ServeHTTP(res http.ResponseWriter, req *http.Request)
 }
 
+// Middleware ...
+type Middleware func(c *Context, next Next)
+
+// Next ...
+type Next func()
+
+// Middlewares ...
+type Middlewares []Middleware
+
+// Router ...
+type Router func(c *Context)
+
 // HTTPServer ...
 type HTTPServer struct {
-	Name       string
-	Listenning bool
-	handler    Handler
+	Name        string
+	Listenning  bool
+	middlewares *Middlewares
+	midLen      int64
+	Routes      []Router
+	Ctx         *Context
+}
+
+// Context ...
+type Context struct {
+	_Request  Request
+	_Response Response
+}
+
+// Request ...
+type Request *http.Request
+
+// Response ...
+type Response http.ResponseWriter
+
+func (s *HTTPServer) routes(c *Context) {
+
+}
+
+// Use ...
+// register a middleware in serv struct
+func (s *HTTPServer) Use(mid Middleware) {
+	s.midLen++
+	*s.middlewares = append(*s.middlewares, mid)
+}
+
+// func (s *HTTPServer) mux(c *Context) Middleware {
+// 	return func(c *Context, next Middleware) {
+
+// 	}
+// }
+
+// // Next ...
+// type Next func()
+
+// Mid ...
+type Mid func(c *Context, n Next)
+
+// Mids ...
+type Mids []Mid
+
+// SetupMiddlewares ...
+func (s *HTTPServer) SetupMiddlewares() {
+	NextIter(s.Ctx, 0, s.middlewares)
+}
+
+// NextIter ...
+func NextIter(ctx *Context, c int, m *Middlewares) {
+	(*m)[c](ctx, func() {
+		c++
+		if c < len(*m) {
+			NextIter(ctx, c, m)
+		}
+	})
 }
 
 // Listen ...
 func (s *HTTPServer) Listen(port string) error {
-	err := http.ListenAndServe(":"+port, s.handler)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return http.ListenAndServe(":"+port, s)
 }
 
-// Router ...
-func (s *HTTPServer) Router() {}
+func (s *HTTPServer) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	s.Ctx._Request = req
+	s.Ctx._Response = res
+	s.SetupMiddlewares()
 
-type router struct{}
+	http.Error(res, "not found", 404)
+}
 
 // Server ...
 func Server() *HTTPServer {
 	// init a server object
 	serv := new(HTTPServer)
 	serv.Name = "mulan"
-	serv.handler = &OnConnect{}
 	fmt.Println(serv)
 
 	return serv
-}
-
-// OnConnect main handler for per request
-type OnConnect struct{}
-
-func (oc *OnConnect) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	fmt.Println(req.Method)
-	http.Error(res, "not implemented", http.StatusNotFound)
 }
 
 // Echo for test
--- mulan_test.go
+++ mulan_test.go
@@ -21,12 +21,52 @@ func TestServer(t *testing.T) {
 			t.Error()
 		}
 	})
+}
 
-	t.Run("try listen", func(t *testing.T) {
-		serv := Server()
-		err := serv.Listen("19990")
-		if err != nil {
-			t.Error(err)
-		}
+func TestNextIter(t *testing.T) {
+	// var mids *Mids
+	// mids := make(*Mids, 3)
+	mids := new(Mids)
+	*mids = append(*mids, func(p int, n Next) {
+		println(p)
+		n()
 	})
+	*mids = append(*mids, func(p int, n Next) {
+		println(p + 1)
+		n()
+	})
+	*mids = append(*mids, func(p int, n Next) {
+		println(p + 2)
+		n()
+	})
+	NextIter(1, 0, mids)
+	// t.Error("foo")
+}
+
+func BenchmarkNextIter100(b *testing.B) {
+	mids := new(Mids)
+	for i := 0; i < 100; i++ {
+		*mids = append(*mids, func(p int, n Next) {
+			// println(p)
+			n()
+		})
+	}
+
+	for n := 0; n < b.N; n++ {
+		NextIter(1, 0, mids)
+	}
+}
+
+func BenchmarkNormalNextIter100(b *testing.B) {
+	var mids Mids
+	for i := 0; i < 100; i++ {
+		mids = append(mids, func(p int, n Next) {
+			// println(p)
+			n()
+		})
+	}
+
+	for n := 0; n < b.N; n++ {
+		NormalNextIter(1, 0, mids)
+	}
 }
